45fd8cd1cbe830b5931e9906a61eb37eb430c02d
Callen <ayarscot@gmail>
2015-05-10 00:32:38 +1000

    add  regular expression
    verify tele num

--- cart.php
+++ cart.php
@@ -63,7 +63,7 @@ echo "<p>Total:".$total."</p>";
   <form action="POST" method="">
     <p>Voucher Code:</p>       <input type="text" pattern="^\d{5}-\d{5}-[A-Z]{2}$" required />
     <p>Email:</p>              <input type="text" pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.([A-Za-z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$" required />
-    <p>MobliePhone number:</p> <input type="text" pattern="^|\+614|04|\s?(\d){4}\s?(\d){4}$" required />
+    <p>MobliePhone number:</p> <input type="text" pattern="^(\+614|04|\(04\))(\s|-)?(\d){4}(\s|-)?(\d){4}$" required />
 	<input type="submit" value="submit" />
   
   </form>
